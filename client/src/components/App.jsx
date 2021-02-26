52f1454017c5ecd040aa1919af72f675bb4fbf1b
Benjamin Wu <benngfour@gmail.com>
2021-02-26 15:54:26 -0800

    Site handles the right url request successfully

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -15,8 +15,15 @@ class App extends React.Component {
   //componentdidmount get request for product id and set state to pass down to components
   componentDidMount() {
     //axios get request to something
-    axios.get('/products/14931')
-    .then((data)=>{console.log(data.data)})
+    // console.log(window.location.pathname);
+    var path = window.location.pathname;
+    console.log('path: ', path);
+    console.log(path.slice(-6));
+    axios.get(path.slice(-6))
+    .then((data)=> {
+      console.log('CLIENT SIDE: ', data)
+      console.log(data.data)
+    })
     .catch((err) => {console.log(err)})
   }
 
--- server/server.js
+++ server/server.js
@@ -8,11 +8,13 @@ const port = 3000;
 
 const staticMiddleware = express.static(path.join(__dirname, '../client/dist'));
 
-app.use('/', staticMiddleware);
+app.use('/products/:q', staticMiddleware);
 
-app.get('/products/:q', (req, res) => {
+app.get('/products/:q/:b', (req, res) => {
+  console.log('SERVER URL=> ', req.url);
   const memCache = [];
   var id = req.params.q
+  console.log('SERVER params==> ', req.params);
   api.fetchProducts(id, (productDetails) => {
     // store the data
     memCache.push(productDetails.data);
