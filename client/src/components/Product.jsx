793b286cb3fc19e3c46ca598a45c54078c0d6dfb
Jacob Peterson <petersonjacobw@gmail.com>
2021-02-28 15:38:26 -0800

    Catching up with Linter

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,40 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import ProductImages from './ProductImages.jsx';
 import ProductOverview from './ProductOverview.jsx';
 import Description from './Description.jsx';
 import FeaturesList from './FeaturesList.jsx';
-import styled from 'styled-components';
 
 const BottomWrapper = styled.div`
   display: flex;
   flex-direction: row;
   padding-top: 5px;
-`
+`;
 
-
-class Product extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      currentStyle: 0,
-    }
-  }
-
-  render () {
-    return (
-      <div>
-        <div className='topLevel'>
-          <ProductImages images={this.props.productStyles}/>
-          <ProductOverview category={this.props.productDetails.category} name={this.props.productDetails.name} price={this.props.productDetails.default_price} styles={this.props.productStyles}/>
-        </div>
-        <BottomWrapper>
-          <Description info={this.props.productDetails} />
-          <FeaturesList features={this.props.productDetails.features}/>
-        </BottomWrapper>
-      </div>
-    );
-  }
-
-}
+const Product = ({ productDetails, productStyles }) => (
+  <div>
+    <div className="topLevel">
+      <ProductImages images={productStyles} />
+      <ProductOverview details={productDetails} styles={productStyles} />
+    </div>
+    <BottomWrapper>
+      <Description info={productDetails} />
+      <FeaturesList features={productDetails.features} />
+    </BottomWrapper>
+  </div>
+);
 
 export default Product;
--- client/src/components/ProductOverview.jsx
+++ client/src/components/ProductOverview.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import Styles from './StylesComponent.jsx';
 
-function ProductOverview({category, name, price, styles}) {
-  return (
-    <div>
-      <div className='productInfo'>
-        <div>Ratings component will go here</div>
-        <div>{category}</div>
-        <div>{name}</div>
-        <div>{price}</div>
-      </div>
-      <Styles/>
-      <div>Buttons and size selectors go here</div>
+const ProductOverview = ({ details, styles }) => (
+  <div>
+    <div className="productInfo">
+      <div>Ratings component will go here</div>
+      <div>{details.category}</div>
+      <div>{details.name}</div>
+      <div>{details.default_price}</div>
     </div>
-  )
-}
-
+    <Styles styles={styles} />
+    <div>Buttons and size selectors go here</div>
+  </div>
+);
 
 export default ProductOverview;
