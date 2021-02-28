3cb14b7617c96cbb393d14f92c2ebf544a3fc8a7
Jacob Peterson <petersonjacobw@gmail.com>
2021-02-28 13:45:43 -0800

    Added Description and Styling
    Product description and slogan have been added
    
    Initial styling of entire bottom div, so that product description and slogan display in line with the features but separated by vertical line.

--- client/src/components/FeaturesList.jsx
+++ client/src/components/FeaturesList.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 const List = styled.ul`
   list-style: none;
+  flex-basis: 40%;
 `
 
 const FeaturesList = ({ features }) => (
--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ProductImages from './ProductImages.jsx';
 import ProductOverview from './ProductOverview.jsx';
+import Description from './Description.jsx';
 import FeaturesList from './FeaturesList.jsx';
+import styled from 'styled-components';
+
+const BottomWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-top: 5px;
+`
+
 
 class Product extends React.Component {
   constructor(props) {
@@ -18,10 +27,10 @@ class Product extends React.Component {
           <ProductImages images={this.props.productStyles}/>
           <ProductOverview category={this.props.productDetails.category} name={this.props.productDetails.name} price={this.props.productDetails.default_price} styles={this.props.productStyles}/>
         </div>
-        <div className='bottomLevel'>
-          <div>{this.props.productDetails.description}</div>
+        <BottomWrapper>
+          <Description info={this.props.productDetails} />
           <FeaturesList features={this.props.productDetails.features}/>
-        </div>
+        </BottomWrapper>
       </div>
     );
   }
