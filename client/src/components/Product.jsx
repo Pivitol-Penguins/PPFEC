70bfba1340fb8e3a7e25731b367d003af7fc98b6
Jacob Peterson <petersonjacobw@gmail.com>
2021-02-28 23:18:06 -0800

    ESLint cleanup on App; rules changes in eslint; SEO updates to index.html

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 
 class Product extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
 
-    }
+    };
   }
 
-  render () {
+  render() {
     // console.log('HERE IS PRODUCT: ', this.props);
     return (
       <div>Product</div>
     );
   }
-
 }
 
 export default Product;
