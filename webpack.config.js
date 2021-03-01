2163e27fa1241c59a435bf5cb61f1df2f431d396
Jacob Peterson <petersonjacobw@gmail.com>
2021-03-01 10:06:09 -0800

    Merging feature branches with local main

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const BrotliGzipPlugin = require('brotli-gzip-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,6 +8,23 @@ module.exports = {
     path: path.resolve(__dirname, './client/dist'),
     filename: 'bundle.js',
   },
+  // plugins: [
+  //   new BrotliGzipPlugin({
+  //       asset: '[path].br[query]',
+  //       algorithm: 'brotli',
+  //       test: /\.(js|css|html|svg)$/,
+  //       threshold: 10240,
+  //       minRatio: 0.8,
+  //       quality: 11,
+  //   }),
+  //   new BrotliGzipPlugin({
+  //       asset: '[path].gz[query]',
+  //       algorithm: 'gzip',
+  //       test: /\.(js|css|html|svg)$/,
+  //       threshold: 10240,
+  //       minRatio: 0.8,
+  //   })
+  // ],
   module: {
     rules: [
       {
