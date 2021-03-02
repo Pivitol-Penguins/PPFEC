24deb7d562934d5d8d2110eabdf2004d65df2364
Jacob Peterson <petersonjacobw@gmail.com>
2021-03-02 12:12:46 -0800

    Logic added to style selector images
    -Deleted StylesThumbnails and merged it into StylesComponents. Changed it into a stateful component and added event listeners to the images, which when clicked, updates the selected style text and paves the way to render the correct data in the sizes drop down and image viewer
    
    -Click handler also added to ViewerThumbnails for future use

--- client/src/components/ProductSubs/StylesComponent.jsx
+++ client/src/components/ProductSubs/StylesComponent.jsx
@@ -1,19 +1,80 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import StyleThumbnails from './StyleThumbnails.jsx';
-
 const StyleWrapper = styled.div`
   margin-top: 10px;
   margin-bottom: 10px;
 `;
 
-const Styles = ({ styles }) => (
-  <StyleWrapper>
-    <div>STYLE</div>
-    <div>SELECTED STYLE</div>
-    <StyleThumbnails images={styles.results} />
-  </StyleWrapper>
-);
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Style = styled.p`
+  margin-left: 1vw;
+  font-weight: bold;
+  color: #424242
+`;
+
+const SelectedStyle = styled.p`
+  margin-left: 1vw;
+  color: #424242
+`;
+
+const Thumbs = styled.div`
+flex-basis: 90%
+display: flex;
+  flex-wrap: wrap;
+  margin: 0;
+`;
+
+const Image = styled.img`
+  flex: 1 0 21%;
+  margin: 5px;
+  border-radius: 100%;
+`;
+
+class Styles extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentStyle: null,
+      style_id: null,
+    };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      currentStyle: this.props.styles.results[0].name,
+      style_id: this.props.styles.results[0].style_id,
+    });
+  }
+
+  clickHandler(event) {
+    this.setState({
+      currentStyle: event.target.alt,
+      style_id: event.target.id,
+    });
+  }
+
+  render() {
+    if (this.state.currentStyle) {
+      return (
+        <StyleWrapper>
+          <TextWrapper>
+            <Style>STYLE &gt;</Style>
+            <SelectedStyle>{this.state.currentStyle.toUpperCase()}</SelectedStyle>
+          </TextWrapper>
+          <Thumbs>
+            { this.props.styles.results.map((image) => <Image onClick={this.clickHandler} key={image.style_id} id={image.style_id} src={image.photos[0].thumbnail_url} alt={image.name} width="70" height="70" />)}
+          </Thumbs>
+        </StyleWrapper>
+      );
+    }
+    return <div>Empty</div>;
+  }
+}
 
 export default Styles;
--- client/src/components/ProductSubs/ViewerThumbnails.jsx
+++ client/src/components/ProductSubs/ViewerThumbnails.jsx
@@ -11,10 +11,42 @@ const Thumbs = styled.div`
   margin: 0;
 `;
 
-const ViewerThumbnails = ({ images }) => (
-  <Thumbs>
-    { images.map((image) => <img key={image.style_id} src={image.photos[0].thumbnail_url} alt="" width="70px" height="70px" />)}
-  </Thumbs>
-);
+const Image = styled.img`
+  margin: 5px;
+`;
+
+class ViewerThumbnails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentStyle: null,
+      style_id: null,
+    };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      currentStyle: this.props.images[0].name,
+      style_id: this.props.images[0].style_id,
+    });
+  }
+
+  clickHandler(event) {
+    this.setState({
+      currentStyle: event.target.alt,
+      style_id: event.target.id,
+    });
+  }
+
+  render() {
+    console.log('viewerthumbs state', this.state);
+    return (
+      <Thumbs>
+        {this.props.images.map((image) => <Image onClick={this.clickHandler} key={image.style_id} id={image.style_id} src={image.photos[0].thumbnail_url} alt={image.name} width="70px" height="70px" />)}
+      </Thumbs>
+    );
+  }
+}
 
 export default ViewerThumbnails;
