045d56c98c1678d5ef699ca2d13f8e73ebd1a72a
Jacob Peterson <petersonjacobw@gmail.com>
2021-03-06 10:50:47 -0800

    Main image nav arrows now disappear when limit reached

--- client/src/components/ProductSubs/ProductImages.jsx
+++ client/src/components/ProductSubs/ProductImages.jsx
@@ -112,9 +112,9 @@ class ProductImages extends React.Component {
     return (
       <Wrapper>
         <ViewerThumbnails viewerIndex={this.state.index} start={this.state.start} end={this.state.end} indexUpdater={this.indexUpdater} images={this.props.images} clickedThumb={this.clickedThumb} id={this.props.id} alt="" />
-        <RightArrow onClick={this.clickNavHandler}><FontAwesome id="1" name="angle-right" size="2x" /></RightArrow>
-        <LeftArrow onClick={this.clickNavHandler}><FontAwesome id="-1" name="angle-left" size="2x" /></LeftArrow>
+        {this.state.index !== 0 && <LeftArrow onClick={this.clickNavHandler}><FontAwesome id="-1" name="angle-left" size="2x" /></LeftArrow> }
         <Image src={this.props.images[this.state.index].url} key={this.props.id} alt="style photograph" />
+        {this.state.index !== this.props.images.length - 1 && <RightArrow onClick={this.clickNavHandler}><FontAwesome id="1" name="angle-right" size="2x" /></RightArrow> }
       </Wrapper>
     );
   }
