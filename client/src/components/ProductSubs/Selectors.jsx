8be4fe9b4b8eab66fbd5784cf5751848543dd487
Jacob Peterson <petersonjacobw@gmail.com>
2021-03-07 15:01:00 -0800

    Formalized logic for dropdowns
    –When a new size is selected, quantity resets
    –If the same size is selected again or user closes dropdown, previous quantity is retained

--- client/src/components/ProductSubs/Selectors.jsx
+++ client/src/components/ProductSubs/Selectors.jsx
@@ -16,11 +16,14 @@ class Selectors extends React.Component {
     super(props);
     this.state = {
       size: null,
+      prevSize: null,
       available: null,
       quantity: null,
+      isSizeSelected: false,
       maxQuantity: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
     };
     this.resetThenSet = this.resetThenSet.bind(this);
+    this.sizeChangeChecker = this.sizeChangeChecker.bind(this);
   }
 
   resetThenSet(quantity, size) {
@@ -29,10 +32,21 @@ class Selectors extends React.Component {
         quantity,
       });
     } else {
-      this.setState({
-        size: [size],
-        available: [quantity],
-      });
+      this.setState((prevState) => ({
+        prevSize: prevState.size,
+        size,
+        available: quantity,
+        isSizeSelected: !prevState.isSizeSelected,
+        headerQuantity: 1,
+      }), () => this.sizeChangeChecker());
+    }
+  }
+
+  sizeChangeChecker() {
+    if (this.state.prevSize !== this.state.size) {
+      this.setState({ isSizeSelected: true });
+    } else {
+      this.setState((prevState) => ({ isSizeSelected: true, headerQuantity: prevState.quantity }));
     }
   }
 
@@ -41,7 +55,17 @@ class Selectors extends React.Component {
     return (
       <Wrapper>
         <SizeDropdown title="SELECT SIZE" list={skus} resetThenSet={this.resetThenSet} />
-        <QuantityDropdown title="-" available={this.state.available} list={this.state.maxQuantity} resetThenSet={this.resetThenSet} />
+        {!this.state.isSizeSelected
+        && <QuantityDropdown title="–" available={this.state.available} list={this.state.maxQuantity} resetThenSet={this.resetThenSet} /> }
+        {this.state.isSizeSelected
+        && (
+        <QuantityDropdown
+          title={this.state.headerQuantity}
+          available={this.state.available}
+          list={this.state.maxQuantity}
+          resetThenSet={this.resetThenSet}
+        />
+        )}
       </Wrapper>
     );
   }
