347fd349194f92aacc0c30fc1c6029da57e4651f
Jacob Peterson <petersonjacobw@gmail.com>
2021-03-07 17:08:20 -0800

    Expanded Image format finalized
    –Expanded image is centered regardless of orientation
    -Expanded image has no border from container regardless of width or orientation
    –Clicking on the image again (for additional zoom) does not increase size of container

--- client/src/components/ProductSubs/ExpandedImage.jsx
+++ client/src/components/ProductSubs/ExpandedImage.jsx
@@ -4,24 +4,21 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  align-items: center;
-  justify-content: center;
+  display: table;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   z-index: 5;
-  max-width: 85vw;
-  max-height: 90vh;
-  margin: auto;
+  max-width: 70vw;
+  max-height: 95vh;
   overflow: hidden;
   box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
 `;
 
 const Expanded = styled.img`
   ${(props) => props.zoomed && 'transform: scale(2.5)'};
-  height: 100%;
-  width: 100%;
+  display: block;
+  max-height: 95vh;
   cursor: ${(props) => (props.zoomed ? 'zoom-out' : 'zoom-in')};
   // background-position-x: ${(props) => props.x};
   // background-position-y: ${(props) => props.y};
@@ -32,8 +29,8 @@ class ExpandedImage extends React.Component {
     super(props);
     this.state = {
       zoomed: false,
-      x: 0,
-      y: 0,
+      // x: 0,
+      // y: 0,
     };
     this.expanded = React.createRef();
     this.imageClickHandler = this.imageClickHandler.bind(this);
