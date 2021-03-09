1c535679883b4de6cda1a2f1a5a6ac16c6fbf7e8
Jacob Peterson <petersonjacobw@gmail.com>
2021-03-09 11:12:09 -0800

    Full test coverage for Price.jsx

--- tests/ProductTests/Price.test.js
+++ tests/ProductTests/Price.test.js
@@ -4,27 +4,42 @@ import { shallow, mount, render } from 'enzyme';
 import Price from '../../client/src/components/ProductSubs/Price.jsx';
 import StylesComponent from '../../client/src/components/ProductSubs/StylesComponent.jsx';
 
-describe('Should show the default price', () => {
+describe('Regular Price with Sale Price', () => {
   const props = {
-    noSale: { price: 40.00, sale: null },
-    sale: { price: 40.00, sale: 30.00 },
+    price: 40.00,
+    sale: 30.00,
   };
   const PriceComponent = mount(<Price {...props} debug />);
 
-  it('Should render price correctly', () => {
-    // mount render the whole dom with styled-component
+  it('Should render correctly', () => {
     expect(PriceComponent).toMatchSnapshot();
   });
 
   it('The form should contain the regular price', () => {
-    expect(PriceComponent.find(<p>{`$${Math.round(props.sale.price)}`}</p>)).toBeTruthy();
+    expect(PriceComponent.find(<p>{`$${Math.round(props.price)}`}</p>)).toBeTruthy();
   });
 
-  it('The form should contain the sale price when present', () => {
-    expect(PriceComponent.find(<p>{`$${Math.round(props.sale.sale)}`}</p>)).toBeTruthy();
+  it('The form should contain the sale price', () => {
+    expect(PriceComponent.find(<p>{`$${Math.round(props.sale)}`}</p>)).toBeTruthy();
+  });
+});
+
+describe('Regular Price without Sale Price', () => {
+  const props = {
+    price: 40.00,
+    sale: null,
+  };
+  const PriceComponent = mount(<Price {...props} debug />);
+
+  it('Should render correctly', () => {
+    expect(PriceComponent).toMatchSnapshot();
   });
 
-  // it('The form should not render a sale price when absent', () => {
-  //   expect(PriceComponent.find(<p>{`$${Math.round(props.noSale.sale)}`}</p>)).toBeFalsy();
-  // });
+  it('The form should contain the regular price', () => {
+    expect(PriceComponent.find(<p>{`$${Math.round(props.price)}`}</p>)).toBeTruthy();
+  });
+
+  it('The form should not contain a sale price', () => {
+    expect(PriceComponent.contains(<p>{`$${Math.round(props.sale)}`}</p>)).toBe(false);
+  });
 });
