435c23af62424876691d0524499b04af9754910b
Nilai Patel <nilaip96@gmail.com>
2021-03-13 15:06:51 -0800

    Added Infinite scroll

--- client/src/components/QnAComponents/QuestionList.jsx
+++ client/src/components/QnAComponents/QuestionList.jsx
@@ -32,17 +32,75 @@ font-family: 'Lato', sans-serif;
 font-weight: 700;
 `;
 
-const QuestionList = (props) => (
-  <QList>
-    {props.items.length === 0 ? <NoR>NOTHING HERE...</NoR> : props.items.map((item) => (
-      <Item
-        item={item}
-        key={item.question_id}
-        id={item.question_id}
-        name={props.name}
-      />
-    ))}
-  </QList>
-);
+const LoadingSpan = styled.span`
+  display: block;
+  color :#42424275;
+
+  &.all-load {
+    display: none;
+  }
+`;
+
+class QuestionList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allLoaded: false,
+      prevY: 0,
+    };
+  }
+
+  componentDidMount() {
+    const options = {
+      root: document.querySelector('#QList'),
+      rootMargin: '0px',
+      threshold: 0,
+    };
+    this.observer = new IntersectionObserver(this.handleObserver.bind(this), options);
+    this.observer.observe(this.loadingRef);
+  }
+
+  handleObserver(entities) {
+    // console.log(entities[0]);
+    const { y } = entities[0].boundingClientRect;
+    if (this.state.prevY > y) {
+      // this.setState({
+      //   loading: true,
+      // });
+      setTimeout(this.props.loadTwo, 1000);
+    }
+    this.setState({ prevY: y });
+    if (this.props.items.length === this.props.data.length) {
+      this.setState({
+        allLoaded: true,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <QList id="QList">
+        {this.props.items.length === 0
+          ? <NoR>NOTHING HERE...</NoR>
+          : this.props.items.map((item) => (
+            <Item
+              item={item}
+              key={item.question_id}
+              id={item.question_id}
+              name={this.props.name}
+            />
+          ))}
+        <LoadingSpan
+          className={(this.state.allLoaded) ? 'all-load' : undefined}
+          // eslint-disable-next-line no-return-assign
+          ref={(loadingRef) => (this.loadingRef = loadingRef)}
+        >
+          Loading...
+        </LoadingSpan>
+      </QList>
+
+    );
+  }
+}
 
 export default QuestionList;
